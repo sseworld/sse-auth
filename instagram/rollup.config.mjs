bb68c3d4b7c8ac68fa65097a4fe9bc345f615edb
SSE World <114757626+sseworld@users.noreply.github.com>
2024-06-14 11:36:03 +0000

    Instagram = Complete Part 2

--- instagram/rollup.config.mjs
+++ instagram/rollup.config.mjs
@@ -9,12 +9,12 @@ export default {
       file: "dist/instagram-login.js",
       format: "umd",
       name: "InstagramLogin",
-      plugins: [commonjs(), babel({ babelHelpers: "bundled" })],
-    },
-    {
-      file: "dist/instagram-login.es.js",
-      format: "es",
-      plugins: [commonjs(), babel({ babelHelpers: "bundled" }), terser()],
+      plugins: [
+        commonjs(),
+        babel({ babelHelpers: "bundled" }),
+        terser({ compress: true }),
+      ],
     },
   ],
+  external: ["react", "react-dom", "prop-types"],
 };
