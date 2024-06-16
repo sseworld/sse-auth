50d859ac1595f8339fb18e0aaa7bf3fab6369722
SSE World <114757626+sseworld@users.noreply.github.com>
2024-06-16 14:23:43 +0000

    Dropbox = Part 2

---
+++ dropbox/generate.js
@@ -0,0 +1,5 @@
+const generateApiDescription2 = require("./src/generate-api-description.js");
+generateApiDescription2();
+// const {decompress} = require("compress-json");
+// const apiJSON = require('./src/api.json');
+// console.log(decompress(apiJSON));
